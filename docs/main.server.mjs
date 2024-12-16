39267f2efa243079063e284ab737458f13c3ac92
TeoSW <149611403+TeoSW@users.noreply.github.com>
2024-12-16 22:00:35 +0200

    aaa

--- docs/main.server.mjs
+++ docs/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-264IUDYR.mjs";import"./chunk-MUCZTE2V.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-SSKVN34M.mjs";import"./chunk-MUCZTE2V.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
